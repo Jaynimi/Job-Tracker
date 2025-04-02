c1029341862b5a76afbafae871781a153a83fedd
Jaynimi <Eyeslikejays@yahoo.com>
2025-04-02 13:11:14 +0200

    auth

--- README.md
+++
@@ -1,2 +0,0 @@
-# Job-Tracker
-A Next JS web app that helps you track your web applications.
---
+++ job-tracker/app/(dashboard)/add-job/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddJobPage = () => {
+  return (
+    <h1>
+      AddJobPage
+    </h1>
+  )
+}
+
+export default AddJobPage
