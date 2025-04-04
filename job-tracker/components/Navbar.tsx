28782d45b0b82b115a4913300201f8b0a0299e36
Jaynimi <Eyeslikejays@yahoo.com>
2025-04-04 18:27:18 +0200

    navbar

--- job-tracker/components/Navbar.tsx
+++ job-tracker/components/Navbar.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import LinksDropdown from "./LinksDropdown";
+import ThemeToggle from "./ThemeToggle";
+import { UserButton } from "@clerk/nextjs";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return <nav className="bg-muted py-4 px-4 sm:px-16 lg:px-24 flex items-center justify-between">
+    <div className="">
+      <LinksDropdown />
+    </div>
+    <div className="flex items-center gap-x-4">
+      <ThemeToggle />
+      <UserButton />
+    </div>
+  </nav>;
 };
 
 export default Navbar;
